b1a92454392da0f86c723d08ebcda280d5c0458b
Peter Kiss <Peter.Kiss@softwire.com>
2018-07-18 12:01:01 +0100

    Basics

--- SupportBank.js
+++ SupportBank.js
@@ -1,41 +1,42 @@
-var readlineSync = require('readline-sync');
+let readlineSync = require('readline-sync');
 
 console.log("Welcome to SupportBank\n \n");
 main();
 function main() {
-    var compile = false;
-    while (compile == false) {
-        var action = readlineSync.question('Would you like to extend the database? (y/n)\n');
-        if ((action == 'y') || (action == 'n')) {
+    let compile = false;
+    var action;
+    while (compile === false) {
+        action = readlineSync.question('Would you like to extend the database? (y/n)\n');
+        if ((action === 'y') || (action === 'n')) {
             compile = true;
         }
         else {console.log("invalid input \n");}
     }
-    if (action == 'y') {
+    if (action === 'y') {
         readFile();
-    }
-    
+    } else { if (action === 'n') {console.log("Bye bye\n");}}
 }
 
 function readFile() {
     var fileName = readlineSync.question('Name of the file to be loaded? \n');
-    var fileFound = false;
-    var breakCommand = false;
-    while ((fileFound == false) && (breakCommand == false)) {
-        var fs = require('fs');
+    let fileFound = false;
+    let breakCommand = false;
+    while ((fileFound === false) && (breakCommand === false)) {
+        let fs = require('fs');
         if (fs.existsSync(fileName)) {
            fileFound = true;
-           console.log("The file is being loaded \n"); 
+           console.log("The file is being loaded \n");
+            
         }
         else {
-            var fileName = readlineSync.question('File not found. Provide valid fileame or type stop \n');
+            fileName = readlineSync.question('File not found. Provide valid fileame or type stop \n');
         }
         
-        if (fileName == "stop") {
+        if (fileName === "stop") {
             breakCommand = true;
         }
     }
     main();
 }
-//var userName = readlineSync.question('Welcome to support\n');
+//let userName = readlineSync.question('Welcome to support\n');
 //console.log('Hi ' + userName + '!');
