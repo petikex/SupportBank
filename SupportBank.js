5d885eaaaf87f611fa09d9f218442f4e80a7b7e9
Peter Kiss <Peter.Kiss@softwire.com>
2018-07-19 14:54:57 +0100

    Final bits of SupportBank

--- SupportBank.js
+++ SupportBank.js
@@ -1,4 +1,15 @@
-const logger = log4js.getLogger('<filename>');
+
+var log4js = require('log4js');
+var moment = require('moment');
+log4js.configure({
+    appenders: {
+        file: { type: 'fileSync', filename: 'logs/debug.log' }
+    },
+    categories: {
+        default: { appenders: ['file'], level: 'debug'}
+    }
+});
+
 console.log('Welcome to SupportBank\n \n');
 let readlineSync = require('readline-sync');
 var transactions = [];
@@ -6,7 +17,8 @@ var numOfAccounts = 0;
 var accounts = [];
 var accountNames = [];
 var numOfTransactions = 0;
-
+var logger;
+const supportedFileFormats = ['csv','son','xml'];
 
 class Account {
     constructor(name,credit) {
@@ -19,7 +31,7 @@ class Account {
         this.transactionSend.push(transaction);
         if ((transaction.amount !== undefined) && (isValidCredit(transaction.amount))) {
             this.credit = this.credit - parseInt(transaction.amount);
-        }
+        } else { logger.warn('Uncrecognised transaction value at: \n',transaction);}
     }
     receive(transaction) {
         this.transactionReceive.push(transaction);
@@ -71,35 +83,30 @@ function progressTransaction(transaction) {
     accounts[accountNames.indexOf(transaction.to)].receive(transaction);
 }
 
+function logProgress(file) {
+    logger = log4js.getLogger(file);
+    logger.info('File',file,'is being loaded.');
+}
+
 function readFile() {
     var fileName = readlineSync.question('Name of the file to be loaded? \n');
     let fileFound = false;
-
+    logProgress(fileName);
     let breakCommand = false;
     while (!fileFound && !breakCommand) {
         let fs = require('fs');
         var temp = fileName;
-        if (fs.existsSync(fileName)) {
+        var extension = fileName.slice(fileName.length - 3, fileName.length);
+        if ((fs.existsSync(fileName)) && (supportedFileFormats.indexOf(extension) !== -1)) {
            fileFound = true;
-           console.log('The file is being loaded \n');
            inputData = fs.readFileSync(temp,'utf8');
-           inputData = inputData.split('\n');
-           var data = [];
-           for (var i = 1; i<inputData.length; i++) {
-               data.push(inputData[i].split(','));
-           }
-           
-           for (var i = 0; i<data.length; i++) {  
-                const temp = data[i];     
-                progressTransaction(new Transaction(temp[0],temp[1],temp[2],temp[3],temp[4]));
-                
-            }
-            console.log('The file has been added');
-            
-            
+           progressFile(inputData, extension);
+           console.log('The file has been added');
         }
         else {
-            fileName = readlineSync.question('File not found. Provide valid fileame or type stop \n');
+            if (supportedFileFormats.indexOf(extension) === -1) {logger.error('Unsupported file format! (',extension,')');}
+            else {  logger.error('File not found at \n', fileName); }
+            fileName = readlineSync.question('Provide valid fileame or type stop \n');
         }
         
         if (fileName === 'stop') {
@@ -107,6 +114,69 @@ function readFile() {
         }
     }
     main();
+}
+
+function progressFile(fileData, extension) {
+    if (extension === 'csv') {
+        progressCsv(fileData);
+    }
+    if (extension === 'son') {
+        progressJson(fileData);
+    }
+    if (extension === 'xml') {
+        progressXml(fileData);
+    }
+}
+
+function progressJson(fileData) {
+    fileData = fileData.slice(1,fileData.length -1);
+    fileData = fileData.split('{');
+    var string;
+    for (var i = 1; i<fileData.length ; i++) {
+        var string = fileData[i];
+        string = string.split('\n');
+        for (var j = 0; j<string.length; j++) {
+            string[j] = string[j].split('"');
+        }
+        var temp = [];
+        for (var j = 1; j<=4; j++) {
+            temp.push(string[j][3]);
+        }
+        temp.push(string[5][2].slice(2,string[5][2].length));
+        progressTransaction(new Transaction(temp[0],temp[1],temp[2],temp[3],temp[4]));
+    }
+}
+
+function progressXml(fileData) {
+    fileData = fileData.split('SupportTransaction');
+    for (var i = 1; i<fileData.length - 1; i = i + 2) {
+        var data = fileData[i];
+        data = data.split('>');
+        var temp = [];
+        temp.push(xmlDate(data[0].slice(7,data[0].length-1)))
+        temp.push(data[7].slice(0,data[7].length-6));
+        temp.push(data[9].slice(0,data[9].length - 4));
+        temp.push(data[2].slice(0,data[2].length - 13));
+        temp.push(data[4].slice(0,data[4].length-7));
+        progressTransaction(new Transaction(temp[0],temp[1],temp[2],temp[3],temp[4]));
+    }
+}
+
+function xmlDate(date) {
+    return moment('1900-01-01').add(date, 'day');
+}
+
+function progressCsv(fileData) {
+    fileData = fileData.split('\n');
+    var data = [];
+    for (var i = 1; i<fileData.length; i++) {
+        data.push(fileData[i].split(','));
+    }       
+    for (var i = 0; i<data.length - 1; i++) {  
+        const temp = data[i];
+        if (temp.length !== 5) {logger.warn('Unrecognised transaction at: \n', temp);}     
+        progressTransaction(new Transaction(temp[0],temp[1],temp[2],temp[3],temp[4]));
+    }
 }
 
 function main() {
